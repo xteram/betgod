88a503f13c930a76730aea3f7b93ff9bdfe37f43
Derrick Afrifa <dev.afrifa@>
2024-10-18 17:38:16 +0000

    Initial Commit

---
+++ betgod/betgodApp.swift
@@ -0,0 +1,17 @@
+//
+//  betgodApp.swift
+//  betgod
+//
+//  Created by XDN Ghana on 14/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct betgodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
